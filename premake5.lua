11da43a4e5ce3eaea2b3ac20e1791e8cc5331ad1
Aleksey Karpov <kiwhyd@gmail.com>
2021-06-17 17:42:34 +0300

    Create premake5.lua

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Osiris"
+	architecture "x64"
+
+	configureations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Osiris"
+	location "Osiris"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17763.0"
+
+		defines
+		{
+			"OSIRIS_PLATFORM_WINDOWS",
+			"OSIRIS_BUILD_DLL"
+		}
+
+		postbuildcommand
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "OSIRIS_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "OSIRIS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "OSIRIS_DIST"
+		optimize "On"
+
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Osiris/vendor/spdlog/include",
+		"Osiris/src"
+	}
+
+	links
+	{
+		"Osiris"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "10.0.17763.0"
+
+		defines
+		{
+			"OSIRIS_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "OSIRIS_DEBUG"
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines "OSIRIS_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist" 
+		defines "OSIRIS_DIST"
+		optimize "On"
